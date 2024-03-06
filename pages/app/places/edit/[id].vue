79f4e63f6b2f4e0db13979f6043b4e1979c1ec8e
yonasisay <yonimelkamu357@gmail.com>
2024-03-06 14:23:57 +0300

    Refactor scroll bar styles and update stepper buttons

--- pages/app/places/edit/[id].vue
+++ pages/app/places/edit/[id].vue
@@ -7,167 +7,167 @@ const placeId = route.params.id || -1;
 
 // ---------------------------- Go Back Handler ----------------------------
 const goPrev = () => {
-  currentStep.value -= 1;
-  router.push({ query: { step: currentStep.value } });
+	currentStep.value -= 1;
+	router.push({ query: { step: currentStep.value } });
 };
 
 // ---------------------------- Go Next Handler ----------------------------
 const goNext = () => {
-  currentStep.value += 1;
-  router.push({ query: { step: currentStep.value } });
+	currentStep.value += 1;
+	router.push({ query: { step: currentStep.value } });
 };
 
 onMounted(() => {
-  if (route.query?.step) {
-    currentStep.value = parseInt(route.query.step);
-  }
+	if (route.query?.step) {
+		currentStep.value = parseInt(route.query.step);
+	}
 });
 watchEffect(() => {
-  if (route.query?.step) {
-    currentStep.value = parseInt(route.query.step);
-  }
+	if (route.query?.step) {
+		currentStep.value = parseInt(route.query.step);
+	}
 });
 
 definePageMeta({
-  title: "Edit Place",
-  layout: "home",
+	title: "Edit Place",
+	layout: "home",
 });
 </script>
 <template>
-  <div class="h-screen">
-    <!-- Top -->
+	<div>
+		<!-- Top -->
 
-    <!-- stepper -->
-    <div class="flex justify-between my-4 h-20">
-      <div
-        class="stepper-button rounded-tl-lg"
-        :class="
-          currentStep == 0
-            ? '!bg-primary-200'
-            : currentStep > 0
-            ? '!bg-primary-600 text-white'
-            : ''
-        "
-      >
-        Basic Info
-      </div>
-      <div
-        class="stepper-button"
-        :class="
-          currentStep == 1
-            ? '!bg-primary-200'
-            : currentStep > 1
-            ? '!bg-primary-600 text-white'
-            : ''
-        "
-      >
-        Location
-      </div>
-      <div
-        class="stepper-button"
-        :class="
-          currentStep == 2
-            ? '!bg-primary-200'
-            : currentStep > 2
-            ? '!bg-primary-600 text-white'
-            : ''
-        "
-      >
-        Amenities
-      </div>
-      <div
-        class="stepper-button"
-        :class="
-          currentStep == 3
-            ? '!bg-primary-200'
-            : currentStep > 3
-            ? '!bg-primary-600 text-white'
-            : ''
-        "
-      >
-        Portfolio
-      </div>
-      <div
-        class="stepper-button"
-        :class="
-          currentStep == 4
-            ? '!bg-primary-200'
-            : currentStep > 4
-            ? '!bg-primary-600 text-white'
-            : ''
-        "
-      >
-        Menu
-      </div>
-      <div
-        class="stepper-button"
-        :class="
-          currentStep == 5
-            ? '!bg-primary-200'
-            : currentStep > 5
-            ? '!bg-primary-600 text-white'
-            : ''
-        "
-      >
-        Pictures
-      </div>
-      <div
-        class="stepper-button rounded-tr-lg"
-        :class="
-          currentStep == 6
-            ? '!bg-primary-200'
-            : currentStep > 6
-            ? '!bg-primary-600 text-white'
-            : ''
-        "
-      >
-        Pricing Plan
-      </div>
-    </div>
-    <div class="min-h-[70%] border flex flex-col justify-between px-5 py-2">
-      <div class="h-full">
-        <PlacesEditBasicInfo
-          @next="goNext"
-          @prev="goPrev"
-          v-if="currentStep == 0"
-          :placeId="placeId"
-        />
-        <PlacesFormLocation
-          @next="goNext"
-          @prev="goPrev"
-          v-if="currentStep == 1"
-          :placeId="placeId"
-        />
-        <PlacesFormAmenities
-          @next="goNext"
-          @prev="goPrev"
-          v-if="currentStep == 2"
-          :placeId="placeId"
-        />
-        <PlacesFormPortfolio
-          @next="goNext"
-          @prev="goPrev"
-          v-if="currentStep == 3"
-          :placeId="placeId"
-        />
-        <PlacesFormMenu
-          @next="goNext"
-          @prev="goPrev"
-          v-if="currentStep == 4"
-          :placeId="placeId"
-        />
-        <PlacesFormPictures
-          @next="goNext"
-          @prev="goPrev"
-          v-if="currentStep == 5"
-          :placeId="placeId"
-        />
-        <PlacesFormPricing
-          @next="goNext"
-          @prev="goPrev"
-          v-if="currentStep == 6"
-        />
-      </div>
-    </div>
-  </div>
+		<!-- stepper -->
+		<div class="flex justify-between h-20 my-4">
+			<div
+				class="rounded-tl-lg stepper-button"
+				:class="
+					currentStep == 0
+						? '!bg-primary-200'
+						: currentStep > 0
+						? '!bg-primary-600 text-white'
+						: ''
+				"
+			>
+				Basic Info
+			</div>
+			<div
+				class="stepper-button"
+				:class="
+					currentStep == 1
+						? '!bg-primary-200'
+						: currentStep > 1
+						? '!bg-primary-600 text-white'
+						: ''
+				"
+			>
+				Location
+			</div>
+			<div
+				class="stepper-button"
+				:class="
+					currentStep == 2
+						? '!bg-primary-200'
+						: currentStep > 2
+						? '!bg-primary-600 text-white'
+						: ''
+				"
+			>
+				Amenities
+			</div>
+			<div
+				class="stepper-button"
+				:class="
+					currentStep == 3
+						? '!bg-primary-200'
+						: currentStep > 3
+						? '!bg-primary-600 text-white'
+						: ''
+				"
+			>
+				Portfolio
+			</div>
+			<div
+				class="stepper-button"
+				:class="
+					currentStep == 4
+						? '!bg-primary-200'
+						: currentStep > 4
+						? '!bg-primary-600 text-white'
+						: ''
+				"
+			>
+				Menu
+			</div>
+			<div
+				class="stepper-button"
+				:class="
+					currentStep == 5
+						? '!bg-primary-200'
+						: currentStep > 5
+						? '!bg-primary-600 text-white'
+						: ''
+				"
+			>
+				Pictures
+			</div>
+			<div
+				class="rounded-tr-lg stepper-button"
+				:class="
+					currentStep == 6
+						? '!bg-primary-200'
+						: currentStep > 6
+						? '!bg-primary-600 text-white'
+						: ''
+				"
+			>
+				Pricing Plan
+			</div>
+		</div>
+		<div class="min-h-[70%] border flex flex-col justify-between px-5 py-2">
+			<div class="h-full">
+				<PlacesEditBasicInfo
+					@next="goNext"
+					@prev="goPrev"
+					v-if="currentStep == 0"
+					:placeId="placeId"
+				/>
+				<PlacesFormLocation
+					@next="goNext"
+					@prev="goPrev"
+					v-if="currentStep == 1"
+					:placeId="placeId"
+				/>
+				<PlacesFormAmenities
+					@next="goNext"
+					@prev="goPrev"
+					v-if="currentStep == 2"
+					:placeId="placeId"
+				/>
+				<PlacesFormPortfolio
+					@next="goNext"
+					@prev="goPrev"
+					v-if="currentStep == 3"
+					:placeId="placeId"
+				/>
+				<PlacesFormMenu
+					@next="goNext"
+					@prev="goPrev"
+					v-if="currentStep == 4"
+					:placeId="placeId"
+				/>
+				<PlacesFormPictures
+					@next="goNext"
+					@prev="goPrev"
+					v-if="currentStep == 5"
+					:placeId="placeId"
+				/>
+				<PlacesFormPricing
+					@next="goNext"
+					@prev="goPrev"
+					v-if="currentStep == 6"
+				/>
+			</div>
+		</div>
+	</div>
 </template>
