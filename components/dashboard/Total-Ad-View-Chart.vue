e8b3fbbe6c8c18e2f527e93bab43d958237859e3
yonasisay <yonimelkamu357@gmail.com>
2024-04-04 16:19:52 +0300

    Refactor UI styles in sidebar and dashboard components

--- components/dashboard/Total-Ad-View-Chart.vue
+++ components/dashboard/Total-Ad-View-Chart.vue
@@ -2,98 +2,98 @@
 import VueApexCharts from "vue3-apexcharts";
 
 const { rawData } = defineProps({
-  rawData: { type: Array, required: true },
+	rawData: { type: Array, required: true },
 });
 
 function formatDate(dateString) {
-  const months = [
-    "Jan",
-    "Feb",
-    "Mar",
-    "Apr",
-    "May",
-    "Jun",
-    "Jul",
-    "Aug",
-    "Sep",
-    "Oct",
-    "Nov",
-    "Dec",
-  ];
+	const months = [
+		"Jan",
+		"Feb",
+		"Mar",
+		"Apr",
+		"May",
+		"Jun",
+		"Jul",
+		"Aug",
+		"Sep",
+		"Oct",
+		"Nov",
+		"Dec",
+	];
 
-  console.log("dateString", dateString);
+	console.log("dateString", dateString);
 
-  const [year, month, day] = dateString
-    .split("-")
-    .map((part) => parseInt(part, 10));
-  const monthName = months[month - 1];
-  return `${monthName} ${day}, ${year}`;
+	const [year, month, day] = dateString
+		.split("-")
+		.map((part) => parseInt(part, 10));
+	const monthName = months[month - 1];
+	return `${monthName} ${day}, ${year}`;
 }
 
 // Area chart option
 var options = {
-  chart: {
-    height: 280,
-    type: "area",
-    toolbar: {
-      show: false,
-    },
-    zoom: {
-      enabled: true,
-    },
-  },
-  dataLabels: {
-    enabled: false,
-  },
-  series: [
-    {
-      name: "Views",
-      data: rawData ? rawData.map((data) => data.count) : [],
-    },
-  ],
-  stroke: {
-    show: false,
-  },
-  fill: {
-    colors: ["#D34553"],
-    type: "gradient",
-    gradient: {
-      shadeIntensity: 1,
-      opacityFrom: 0.7,
-      opacityTo: 1,
-      stops: [0, 90, 100],
-    },
-  },
-  xaxis: {
-    type: "category",
-    categories: rawData ? rawData.map((data) => formatDate(data.date)) : [],
-  },
-  yaxis: {
-    show: false,
-  },
-  tooltip: {
-    enabled: true,
-    custom: function ({ series, seriesIndex, dataPointIndex, w }) {
-      // console.log(w);
-      return (
-        '<div class="tooltip-container">' +
-        "<div class='tooltip-value'>" +
-        series[seriesIndex][dataPointIndex] +
-        " " +
-        w.globals.seriesNames[seriesIndex] +
-        "</div>" +
-        "<div class='tooltip-date'>" +
-        w.globals.categoryLabels[dataPointIndex] +
-        "</div>" +
-        "</div>"
-      );
-    },
-  },
+	chart: {
+		height: 280,
+		type: "area",
+		toolbar: {
+			show: false,
+		},
+		zoom: {
+			enabled: true,
+		},
+	},
+	dataLabels: {
+		enabled: false,
+	},
+	series: [
+		{
+			name: "Views",
+			data: rawData ? rawData.map((data) => data.count) : [],
+		},
+	],
+	stroke: {
+		show: false,
+	},
+	fill: {
+		colors: ["#D34553"],
+		type: "gradient",
+		gradient: {
+			shadeIntensity: 1,
+			opacityFrom: 0.7,
+			opacityTo: 1,
+			stops: [0, 90, 100],
+		},
+	},
+	xaxis: {
+		type: "category",
+		categories: rawData ? rawData.map((data) => formatDate(data.date)) : [],
+	},
+	yaxis: {
+		show: false,
+	},
+	tooltip: {
+		enabled: true,
+		custom: function ({ series, seriesIndex, dataPointIndex, w }) {
+			// console.log(w);
+			return (
+				'<div class="tooltip-container">' +
+				"<div class='tooltip-value'>" +
+				series[seriesIndex][dataPointIndex] +
+				" " +
+				w.globals.seriesNames[seriesIndex] +
+				"</div>" +
+				"<div class='tooltip-date'>" +
+				w.globals.categoryLabels[dataPointIndex] +
+				"</div>" +
+				"</div>"
+			);
+		},
+	},
 };
 </script>
 <template>
-  <div class="box-content p-5 shadow rounded-xl">
-    <h2 class="text-xl font-medium text-justify">Total AD View</h2>
-    <VueApexCharts :options="options" :series="options.series" />
-  </div>
+	<div class="box-content p-5 shadow rounded-xl">
+		<h2 class="text-xl font-medium text-justify">Total AD View</h2>
+		<VueApexCharts :options="options" :series="options.series" />
+	</div>
 </template>
--- components/modals/Billing-Invoice.vue
+++ components/modals/Billing-Invoice.vue
@@ -37,18 +37,19 @@ invoiceOnResult((result) => {
 	}
 });
 
-const invoiceDiv = document.getElementById("invoiceDiv");
-
 function generatePDF() {
-	const opt = {
-		margin: 0,
-		filename: "converted.pdf",
-		image: { type: "jpeg", quality: 0.98 },
-		html2canvas: { scale: 2 },
-		jsPDF: { unit: "mm", format: "a4", orientation: "portrait" },
-	};
-
-	html2pdf().set(opt).from(invoiceDiv).save();
+	html2pdf()
+		.from(document.getElementById("invoiceDiv"))
+		.set({
+			margin: 1,
+			filename: "Shegergebeta-Invoice.pdf",
+			html2canvas: {
+				dpi: 300,
+				letterRendering: true,
+				useCORS: true,
+			},
+		})
+		.save();
 }
 
 /*----------------------------Toggle Full Screen ----------------------------------*/
@@ -86,192 +87,199 @@ const open = computed({
 				</div>
 				<div class="grid grid-cols-5 mt-4 gap-x-4">
 					<!--  -----------------------Left side---------------- -->
-					<div
-						id="invoiceDiv"
-						class="col-span-3 pb-2 space-y-3 overflow-hidden border rounded-md"
-					>
-						<div class="grid items-center grid-cols-4 p-10 bg-primary-50">
-							<!-- ---------------------Sponsors--------------------- -->
-							<div class="flex flex-col col-span-3 space-y-2">
-								<div class="items-center secondary-flex-row">
-									<button class="w-10 h-10 rounded-full bg-primary-600">
-										<Icon name="uil:phone" class="text-white"></Icon>
-									</button>
+					<div class="col-span-3 border rounded-md">
+						<div id="invoiceDiv" class="pb-2 space-y-3 overflow-hidden">
+							<div class="grid items-center grid-cols-4 p-10 bg-primary-50">
+								<!-- ---------------------Sponsors--------------------- -->
+								<div class="flex flex-col col-span-3 space-y-2">
+									<div class="items-center secondary-flex-row">
+										<button class="w-10 h-10 rounded-full bg-primary-600">
+											<Icon name="uil:phone" class="text-white"></Icon>
+										</button>
 
-									<div>
-										<p class="secondary-text">+251 123 456 8677</p>
-										<p class="secondary-text">+251 123 456 8677</p>
+										<div>
+											<p class="secondary-text">+251 123 456 8677</p>
+											<p class="secondary-text">+251 123 456 8677</p>
+										</div>
+									</div>
+									<div class="items-center secondary-flex-row">
+										<button class="w-10 h-10 rounded-full bg-primary-600">
+											<Icon name="uil:phone" class="text-white"></Icon>
+										</button>
+
+										<p class="secondary-text">info@shegergebeta.com</p>
+									</div>
+									<div class="items-center secondary-flex-row">
+										<button class="w-10 h-10 rounded-full bg-primary-600">
+											<Icon name="uil:phone" class="text-white"></Icon>
+										</button>
+
+										<p class="secondary-text w-[70%]">
+											Afomi Building 3rd floor, Bole road, Behind Friendship
+											Mall Addis Ababa, Ethiopia
+										</p>
 									</div>
 								</div>
-								<div class="items-center secondary-flex-row">
-									<button class="w-10 h-10 rounded-full bg-primary-600">
-										<Icon name="uil:phone" class="text-white"></Icon>
-									</button>
 
-									<p class="secondary-text">info@shegergebeta.com</p>
-								</div>
-								<div class="items-center secondary-flex-row">
-									<button class="w-10 h-10 rounded-full bg-primary-600">
-										<Icon name="uil:phone" class="text-white"></Icon>
-									</button>
-
-									<p class="secondary-text w-[70%]">
-										Afomi Building 3rd floor, Bole road, Behind Friendship Mall
-										Addis Ababa, Ethiopia
-									</p>
-								</div>
-							</div>
-
-							<!-- --------------Logo---------------- -->
-							<div class="flex justify-end secondary-flex-row">
-								<div>
-									<img
-										src="/images/static/large-size-logo.png"
-										alt=" logo"
-										class="object-cover w-24 h-24"
-									/>
-								</div>
-								<div
-									class="pl-3 text-2xl font-semibold border-l-2 border-white text-primary-600"
-								>
-									<p>Sheger</p>
-									<p>Gebeta</p>
+								<!-- --------------Logo---------------- -->
+								<div class="flex justify-end secondary-flex-row">
+									<div>
+										<img
+											src="/images/static/large-size-logo.png"
+											alt=" logo"
+											class="object-cover w-24 h-24"
+										/>
+									</div>
+									<div
+										class="pl-3 text-2xl font-semibold border-l-2 border-white text-primary-600"
+									>
+										<p>Sheger</p>
+										<p>Gebeta</p>
+									</div>
 								</div>
 							</div>
-						</div>
-						<!-- ------------------Bill data--------------------- -->
-						<div class="relative h-full px-4">
-							<!-- -----------------Section 1----------- -->
+							<!-- ------------------Bill data--------------------- -->
+							<div class="relative h-full px-4">
+								<!-- -----------------Section 1----------- -->
 
-							<div class="flex">
-								<div class="flex flex-col space-y-3 w-[60%]">
-									<p class="secondary-text">
-										Bill To :
-										<span class="font-medium">{{
-											invoice?.payment?.billing_address?.place?.name
-										}}</span>
-									</p>
-									<p class="secondary-text">
-										Address :
-										<span class="font-medium"
-											>{{ invoice?.payment?.billing_address?.city?.name }}
+								<div class="flex">
+									<div class="flex flex-col space-y-3 w-[60%]">
+										<p class="secondary-text">
+											Bill To :
+											<span class="font-medium">{{
+												invoice?.payment?.billing_address?.place?.name
+											}}</span>
+										</p>
+										<p class="secondary-text">
+											Address :
+											<span class="font-medium"
+												>{{ invoice?.payment?.billing_address?.city?.name }}
 
-											{{ invoice?.payment?.billing_address?.area?.name }}</span
-										>
-									</p>
-
-									<p class="secondary-text">
-										Phone :
-										<span class="font-medium">{{
-											invoice?.payment?.billing_address?.place
-												?.contactPhoneNumber
-										}}</span>
-									</p>
-									<p class="secondary-text">
-										Email :
-										<span class="font-medium">{{
-											invoice?.payment?.billing_address?.place?.contactEmail
-										}}</span>
-									</p>
-								</div>
-
-								<!-- ----- -->
-
-								<div class="flex flex-col space-y-5">
-									<p class="space-x-2 secondary-text">
-										<span class="font-light underline"> Date </span>:
-										<span class="font-medium">{{
-											invoice?.payment?.paymentDate
-										}}</span>
-									</p>
-									<p class="flex space-x-2 secondary-text whitespace-nowrap">
-										<span class="font-light underline">Invoice Number </span>
-										:
-										<span class="font-medium">
-											{{ invoice?.invoiceNumber }}</span
-										>
-									</p>
-
-									<p class="space-x-2 secondary-text">
-										<span class="font-light underline"> Tin Number </span>:
-										<span class="font-medium">{{
-											invoice?.payment?.billing_address?.place?.tinNumber
-										}}</span>
-									</p>
-								</div>
-							</div>
-
-							<div class="w-full my-2 h-[4px] bg-primary-600"></div>
-
-							<div class="border-b">
-								<table class="w-full">
-									<tr class="text-left text-primary-600">
-										<th class="py-4">Description</th>
-										<th class="py-4">Price</th>
-										<th class="py-4">Qty</th>
-										<th class="py-4">Amount</th>
-									</tr>
-									<tr class="text-lg font-medium">
-										<td>
-											{{
-												invoice?.payment?.pricing_plan_frequency?.pricingPlan
-													?.title
-											}}
-											<span class="text-sm font-light">
-												/{{
-													invoice?.payment?.pricing_plan_frequency?.frequency
+												{{
+													invoice?.payment?.billing_address?.area?.name
 												}}</span
 											>
-										</td>
-										<td>
-											{{ invoice.subTotal }}
-										</td>
-										<td>1</td>
-										<td>{{ invoice.total }}</td>
-									</tr>
-								</table>
-							</div>
+										</p>
 
-							<!-- ---------------footer---------------- -->
+										<p class="secondary-text">
+											Phone :
+											<span class="font-medium">{{
+												invoice?.payment?.billing_address?.place
+													?.contactPhoneNumber
+											}}</span>
+										</p>
+										<p class="secondary-text">
+											Email :
+											<span class="font-medium">{{
+												invoice?.payment?.billing_address?.place?.contactEmail
+											}}</span>
+										</p>
+									</div>
 
-							<div class="flex justify-end py-4">
-								<div class="grid grid-cols-2 gap-y-2">
-									<p class="text-xl font-medium text-right text-primary-600">
-										Subtotal
-									</p>
-									<p
-										class="max-w-[8rem] flex items-center gap-x-2 mx-auto secondary-text lg:text-lg text-right"
-									>
-										{{ invoice.subTotal }}<span class="text-sm">Br.</span>
-									</p>
+									<!-- ----- -->
 
-									<p class="text-xl font-medium text-right text-primary-600">
-										Vat
-									</p>
-									<p
-										class="max-w-[8rem] flex items-center gap-x-2 mx-auto secondary-text lg:text-lg text-right"
-									>
-										{{ invoice.vat }}<span class="text-sm">Br.</span>
-									</p>
+									<div class="flex flex-col space-y-5">
+										<p class="space-x-2 secondary-text">
+											<span class="font-light underline underline-offset-4">
+												Date </span
+											>:
+											<span class="font-medium">{{
+												invoice?.payment?.paymentDate
+											}}</span>
+										</p>
+										<p class="flex space-x-2 secondary-text whitespace-nowrap">
+											<span class="font-light underline underline-offset-4"
+												>Invoice Number
+											</span>
+											:
+											<span class="font-medium">
+												{{ invoice?.invoiceNumber }}</span
+											>
+										</p>
 
-									<p class="text-xl font-medium text-right text-primary-600">
-										Total Amount Due
-									</p>
-									<p
-										class="text-primary-600 max-w-[8rem] mx-auto flex items-center gap-x-2 lg:text-lg text-right"
-									>
-										{{ invoice.total }} <span class="text-sm">Br.</span>
-									</p>
+										<p class="space-x-2 secondary-text">
+											<span class="font-light underline underline-offset-4">
+												Tin Number </span
+											>:
+											<span class="font-medium">{{
+												invoice?.payment?.billing_address?.place?.tinNumber
+											}}</span>
+										</p>
+									</div>
 								</div>
-							</div>
 
-							<div class="mt-6">
-								<p class="secondary-text">Invoice Terms</p>
-								<div class="flex justify-between mt-2">
-									<p class="max-w-sm secondary-text">
-										Please pay your invoice in person at the head office of
-										Sheger Gebeta before the due date.
-									</p>
+								<div class="w-full my-2 h-[4px] bg-primary-600"></div>
+
+								<div class="pb-4 border-b">
+									<table class="w-full">
+										<tr class="text-left text-primary-600">
+											<th class="py-4">Description</th>
+											<th class="py-4">Price</th>
+											<th class="py-4">Qty</th>
+											<th class="py-4">Amount</th>
+										</tr>
+										<tr class="text-lg font-medium">
+											<td>
+												{{
+													invoice?.payment?.pricing_plan_frequency?.pricingPlan
+														?.title
+												}}
+												<span class="text-sm font-light">
+													/{{
+														invoice?.payment?.pricing_plan_frequency?.frequency
+													}}</span
+												>
+											</td>
+											<td>
+												{{ invoice.subTotal }}
+											</td>
+											<td>1</td>
+											<td>{{ invoice.total }}</td>
+										</tr>
+									</table>
+								</div>
+
+								<!-- ---------------footer---------------- -->
+
+								<div class="flex justify-end py-4">
+									<div class="grid grid-cols-2 gap-y-2">
+										<p class="text-xl font-medium text-right text-primary-600">
+											Subtotal
+										</p>
+										<p
+											class="max-w-[8rem] flex items-center gap-x-2 mx-auto secondary-text lg:text-lg text-right"
+										>
+											{{ invoice.subTotal }}<span class="text-sm">Br.</span>
+										</p>
+
+										<p class="text-xl font-medium text-right text-primary-600">
+											Vat
+										</p>
+										<p
+											class="max-w-[8rem] flex items-center gap-x-2 mx-auto secondary-text lg:text-lg text-right"
+										>
+											{{ invoice.vat }}<span class="text-sm">Br.</span>
+										</p>
+
+										<p class="text-xl font-medium text-right text-primary-600">
+											Total Amount Due
+										</p>
+										<p
+											class="text-primary-600 max-w-[8rem] mx-auto flex items-center gap-x-2 lg:text-lg text-right"
+										>
+											{{ invoice.total }} <span class="text-sm">Br.</span>
+										</p>
+									</div>
+								</div>
+
+								<div class="mt-6">
+									<p class="secondary-text">Invoice Terms</p>
+									<div class="flex justify-between mt-2">
+										<p class="max-w-sm secondary-text">
+											Please pay your invoice in person at the head office of
+											Sheger Gebeta before the due date.
+										</p>
+									</div>
 								</div>
 							</div>
 						</div>
@@ -319,7 +327,7 @@ const open = computed({
 									v-if="invoice.payment.recieptUrl"
 									:src="invoice?.payment?.recieptUrl"
 									alt=" payment file"
-									class="object-cover h-full rounded-md"
+									class="object-cover max-h-[500px] h-full rounded-md"
 								/>
 								<div v-else class="space-y-6 text-center text-gray-500">
 									<Icon
