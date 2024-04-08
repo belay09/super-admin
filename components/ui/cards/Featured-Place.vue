e34150a167c1c18f72d79ec6338f95ef57eda19b
yonasisay <yonimelkamu357@gmail.com>
2024-04-08 16:42:31 +0300

    Update sidebar and stepper buttons styling

--- components/ui/cards/Featured-Place.vue
+++ components/ui/cards/Featured-Place.vue
@@ -10,22 +10,22 @@ const { notify } = useNotify();
 const router = useRouter();
 const emit = defineEmits(["toggleSelect", "edit"]);
 const props = defineProps({
-  place: {
-    type: Object,
-    required: true,
-  },
-  isFeatured: {
-    type: Boolean,
-    default: false,
-  },
-  isAdd: {
-    type: Boolean,
-    default: false,
-  },
-  featuredValue: {
-    type: String,
-    required: true,
-  },
+	place: {
+		type: Object,
+		required: true,
+	},
+	isFeatured: {
+		type: Boolean,
+		default: false,
+	},
+	isAdd: {
+		type: Boolean,
+		default: false,
+	},
+	featuredValue: {
+		type: String,
+		required: true,
+	},
 });
 
 /***---------------------------Main location---------------------- */
@@ -33,41 +33,41 @@ const mainLocation = ref(null);
 
 /**--------------------Convert the feature value to name------------- */
 function featuredPlaceValue(value) {
-  if (value === "RECENTLY_OPENED_PLACE") {
-    return {
-      name: "Recently Opened",
-      class: " text-[#47FF6F]",
-    };
-  } else if (value === "WEEKLY_RECOMMENDED_PLACE") {
-    return {
-      name: "Recommendation",
-      class: "text-[#47FF6F]",
-    };
-  } else if (value === "SEASONAL_PLACE") {
-    return {
-      name: "Seasonal",
-      class: "text-[#47FF6F]",
-    };
-  }
+	if (value === "RECENTLY_OPENED_PLACE") {
+		return {
+			name: "Recently Opened",
+			class: " text-[#47FF6F]",
+		};
+	} else if (value === "WEEKLY_RECOMMENDED_PLACE") {
+		return {
+			name: "Recommendation",
+			class: "text-[#47FF6F]",
+		};
+	} else if (value === "SEASONAL_PLACE") {
+		return {
+			name: "Seasonal",
+			class: "text-[#47FF6F]",
+		};
+	}
 }
 
 /**---------------------------Select place to make it featured-------------- */
 const isSelected = ref(false);
 function handleCardClick() {
-  if (!props.isAdd) {
-    router.push(`/app/places/${props.place.id}`);
-    return;
-  }
-  isSelected.value = !isSelected.value;
-  emit("toggleSelect", props.place.id); // add the featured place
+	if (!props.isAdd) {
+		router.push(`/app/places/${props.place.id}`);
+		return;
+	}
+	isSelected.value = !isSelected.value;
+	emit("toggleSelect", props.place.id); // add the featured place
 }
 
 /**---------------------------Get the other featured type data by using id------------------ */
 function getFeaturedPlace() {
-  const featuredPlace = props.place.featured_places.find(
-    (featured) => featured.featured_place_type.value === props.featuredValue
-  );
-  return featuredPlace;
+	const featuredPlace = props.place.featured_places.find(
+		(featured) => featured.featured_place_type.value === props.featuredValue
+	);
+	return featuredPlace;
 }
 
 /**--------------------------Click outside------------------- */
@@ -76,60 +76,60 @@ onClickOutside(featureActionsContainer, (e) => (showMoreAction.value = false));
 
 /**------------------------------Handle edit--------------- */
 const {
-  mutate: editMutate,
-  onDone: editDone,
-  onError: editError,
-  loading: editLoading,
+	mutate: editMutate,
+	onDone: editDone,
+	onError: editError,
+	loading: editLoading,
 } = authMutation(editMutation);
 
 /**-------------------------Remove from featured---------------- */
 const removeFromFeatured = () => {
-  let input = {
-    isActive: false,
-  };
-  editMutate({ input, id: getFeaturedPlace().id });
+	let input = {
+		isActive: false,
+	};
+	editMutate({ input, id: getFeaturedPlace().id });
 };
 
 /**--------------------------Extend date------------------------ */
 const endDate = ref("");
 const extendDate = handleSubmit(() => {
-  let input = {
-    endDate: endDate.value,
-  };
-  editMutate({ input, id: getFeaturedPlace().id });
+	let input = {
+		endDate: endDate.value,
+	};
+	editMutate({ input, id: getFeaturedPlace().id });
 });
 
 /***-----------------------Edit on done------------------------ */
 editDone(() => {
-  showMoreAction.value = false;
-  showExtendEndDateModal.value = false;
-  emit("edit");
-  notify({
-    title: "Updated  successfully",
-    description: "Updated  successfully",
-    type: "error",
-    borderClass: "border-l-8 border-green-300",
-  });
+	showMoreAction.value = false;
+	showExtendEndDateModal.value = false;
+	emit("edit");
+	notify({
+		title: "Updated  successfully",
+		description: "Updated  successfully",
+		type: "error",
+		borderClass: "border-l-8 border-green-300",
+	});
 });
 
 editError((error) => {
-  showMoreAction.value = false;
-  notify({
-    title: "Some thing went wrong",
-    description: error.message,
-    type: "error",
-    borderClass: "border-l-8 border-red-300",
-  });
+	showMoreAction.value = false;
+	notify({
+		title: "Some thing went wrong",
+		description: error.message,
+		type: "error",
+		borderClass: "border-l-8 border-red-300",
+	});
 });
 
 onMounted(() => {
-  endDate.value = getFeaturedPlace().endDate;
-  isSelected.value = props.isFeatured;
+	endDate.value = getFeaturedPlace().endDate;
+	isSelected.value = props.isFeatured;
 
-  mainLocation.value =
-    props.place.placeLocations.find(
-      (placeLocation) => placeLocation.isMainLocation
-    ) || props.place.placeLocations[0];
+	mainLocation.value =
+		props.place.placeLocations.find(
+			(placeLocation) => placeLocation.isMainLocation
+		) || props.place.placeLocations[0];
 });
 
 /***-----------------Modals--------------------------- */
@@ -138,207 +138,207 @@ const showRemoveFromFeaturedModal = ref(false);
 const showMoreAction = ref(false);
 </script>
 <template>
-  <!-- -------------------Extend end date modal--------------- -->
-  <Modals-Modal
-    body-class="!max-w-[30rem]"
-    :autoClose="true"
-    :modelValue="showExtendEndDateModal"
-  >
-    <template #header>
-      <div class="flex items-center justify-between pb-4">
-        <h3 class="text-lg font-medium text-gray-900">Extend Deadline</h3>
-        <button @click="showExtendEndDateModal = false">
-          <Icon name="system-uicons:close" class="text-4xl" />
-        </button>
-      </div>
-    </template>
-    <template #content>
-      <div>
-        <form
-          @submit.prevent="extendDate"
-          class="flex flex-col gap-y-3"
-          action=""
-        >
-          <!-- ---------------------Date------------- -->
-          <HDatePickers
-            id="end_date"
-            name="end_date"
-            rules="required"
-            label="End Date"
-            trailing-icon="uil:calender"
-            trailing-icon-class=""
-            class="w-full"
-            v-model="endDate"
-          ></HDatePickers>
+	<!-- -------------------Extend end date modal--------------- -->
+	<Modals-Modal
+		body-class="!max-w-[30rem]"
+		:autoClose="true"
+		:modelValue="showExtendEndDateModal"
+	>
+		<template #header>
+			<div class="flex items-center justify-between pb-4">
+				<h3 class="text-lg font-medium text-gray-900">Extend Deadline</h3>
+				<button @click="showExtendEndDateModal = false">
+					<Icon name="system-uicons:close" class="text-4xl" />
+				</button>
+			</div>
+		</template>
+		<template #content>
+			<div>
+				<form
+					@submit.prevent="extendDate"
+					class="flex flex-col gap-y-3"
+					action=""
+				>
+					<!-- ---------------------Date------------- -->
+					<HDatePickers
+						id="end_date"
+						name="end_date"
+						rules="required"
+						label="End Date"
+						trailing-icon="uil:calender"
+						trailing-icon-class=""
+						class="w-full"
+						v-model="endDate"
+					></HDatePickers>
 
-          <!-- ------------------Update button---------------- -->
-          <button
-            @click="showConfirmationModal = true"
-            class="justify-center border primary-button secondary-text"
-          >
-            Update Deadline
-            <Icon
-              v-if="editLoading"
-              name="eos-icons:bubble-loading"
-              class="text-3xl text-red-600"
-            />
-          </button>
-        </form>
-      </div>
-    </template>
-  </Modals-Modal>
+					<!-- ------------------Update button---------------- -->
+					<button
+						@click="showConfirmationModal = true"
+						class="justify-center border primary-button secondary-text"
+					>
+						Update Deadline
+						<Icon
+							v-if="editLoading"
+							name="eos-icons:bubble-loading"
+							class="text-3xl text-red-600"
+						/>
+					</button>
+				</form>
+			</div>
+		</template>
+	</Modals-Modal>
 
-  <!-- -----------------Remove from featured ---------------- -->
+	<!-- -----------------Remove from featured ---------------- -->
 
-  <ModalsConfirmation
-    @confirm="removeFromFeatured"
-    v-model="showRemoveFromFeaturedModal"
-    :title="`Remove from ${featuredPlaceValue(featuredValue).name}`"
-    v-bind:sure-question="`Are you sure you want to remove ${
-      featuredPlaceValue(featuredValue).name
-    } ?`"
-    description=""
-  ></ModalsConfirmation>
-  <!-- -----------------------Card---------------- -->
-  <div
-    @click="handleCardClick"
-    class="flex flex-col border max-w-lg p-[25px] rounded-xl text-sm hover:cursor-pointer"
-    :class="isSelected ? 'border-primary-600' : ''"
-  >
-    <!-- Header -->
-    <div class="flex items-start justify-between">
-      <div class="flex gap-3 w-full">
-        <!-- Logo -->
-        <div class="w-20 h-20 self-start shrink-0 mr-1">
-          <!-- Image -->
-          <img
-            :src="place.light_logo?.url"
-            alt=""
-            class="object-cover w-full h-full rounded-xl"
-          />
-        </div>
-        <!-- Middle -->
-        <div class="flex flex-col gap-2 w-full">
-          <!-- -----------------Name ------------------->
-          <div class="flex justify-between">
-            <h1 class="text-xl font-medium">{{ place.name }}</h1>
-            <!---------------- Option Icon---------------- -->
-            <div v-if="isSelected && isAdd" class="hover:cursor-pointer">
-              <Icon name="icons8:checked" class="text-2xl text-primary-600" />
-            </div>
-            <div v-if="!isAdd" class="relative">
-              <button
-                @click.stop="showMoreAction = !showMoreAction"
-                class="hover:cursor-pointer"
-              >
-                <Icon name="iwwa:option" class="text-3xl" />
-              </button>
+	<ModalsConfirmation
+		@confirm="removeFromFeatured"
+		v-model="showRemoveFromFeaturedModal"
+		:title="`Remove from ${featuredPlaceValue(featuredValue).name}`"
+		v-bind:sure-question="`Are you sure you want to remove ${
+			featuredPlaceValue(featuredValue).name
+		} ?`"
+		description=""
+	></ModalsConfirmation>
+	<!-- -----------------------Card---------------- -->
+	<div
+		@click="handleCardClick"
+		class="flex flex-col border max-w-lg hover:border-primary-600 hover:shadow-md p-[25px] rounded-xl text-sm hover:cursor-pointer"
+		:class="isSelected ? 'border-primary-600' : ''"
+	>
+		<!-- Header -->
+		<div class="flex items-start justify-between">
+			<div class="flex w-full gap-3">
+				<!-- Logo -->
+				<div class="self-start w-20 h-20 mr-1 shrink-0">
+					<!-- Image -->
+					<img
+						:src="place.light_logo?.url"
+						alt=""
+						class="object-cover w-full h-full rounded-xl"
+					/>
+				</div>
+				<!-- Middle -->
+				<div class="flex flex-col w-full gap-2">
+					<!-- -----------------Name ------------------->
+					<div class="flex justify-between">
+						<h1 class="text-xl font-medium">{{ place.name }}</h1>
+						<!---------------- Option Icon---------------- -->
+						<div v-if="isSelected && isAdd" class="hover:cursor-pointer">
+							<Icon name="icons8:checked" class="text-2xl text-primary-600" />
+						</div>
+						<div v-if="!isAdd" class="relative">
+							<button
+								@click.stop="showMoreAction = !showMoreAction"
+								class="hover:cursor-pointer"
+							>
+								<Icon name="iwwa:option" class="text-3xl" />
+							</button>
 
-              <!-- -----------------------Extend date and Remove from feature --------------- -->
-              <div
-                ref="featureActionsContainer"
-                v-if="showMoreAction"
-                class="absolute top-7 left-0 w-72 flex flex-col gap-y-4 shadow-xl bg-white rounded-lg p-5"
-              >
-                <div
-                  class="flex gap-3 items-center"
-                  @click.stop="showExtendEndDateModal = true"
-                >
-                  <Icon
-                    name="heroicons:calendar-days-16-solid"
-                    class="text-2xl shrink-0"
-                  />
-                  <p class="text-sm">Extend Deadline</p>
-                </div>
-                <button
-                  @click.stop="showRemoveFromFeaturedModal = true"
-                  class="flex gap-3 items-center"
-                >
-                  <Icon
-                    name="heroicons:minus-16-solid"
-                    class="text-2xl shrink-0"
-                  />
-                  <p class="text-sm text-primary-600 whitespace-nowrap">
-                    Remove from {{ featuredPlaceValue(featuredValue)?.name }}
-                  </p>
-                </button>
-              </div>
-            </div>
-          </div>
-          <!-- status -->
-          <div class="flex flex-wrap gap-x-4 gap-y-2">
-            <div class="bg-blue-300 px-2 py-1 rounded-sm">
-              <p class="text-primary-600 font-medium capitalize">
-                {{ place.type?.toLowerCase() }}
-              </p>
-            </div>
-            <div
-              v-for="featuredType in place?.featured_places"
-              class="px-2 py-1 rounded-sm"
-              :class="
-                featuredType.isActive
-                  ? 'bg-green-200 text-primary-600'
-                  : 'bg-primary-100 text-primary-600'
-              "
-            >
-              <p class="font-medium">
-                {{
-                  featuredPlaceValue(featuredType.featured_place_type?.value)
-                    .name
-                }}
-              </p>
-            </div>
-          </div>
-        </div>
-      </div>
-    </div>
-    <!-- -------------------Body ---------------------------->
-    <div class="flex flex-col gap-4 my-2 text-sheger-gray-100">
-      <!----------------- Rating , Review   and Like -------------->
-      <CommonReviewRatingLike
-        :rating="place.place_aggregate_summary?.avgRating || 0"
-        :like="place.place_aggregate_summary?.sumLikes || 0"
-        :review="place.placeReviewsAggregate?.aggregate?.count || 0"
-      ></CommonReviewRatingLike>
-      <!-------------------- Location------------------ -->
-      <div v-if="mainLocation" class="flex items-center gap-3">
-        <Icon name="carbon:map" class="h-5 w-5" />
+							<!-- -----------------------Extend date and Remove from feature --------------- -->
+							<div
+								ref="featureActionsContainer"
+								v-if="showMoreAction"
+								class="absolute left-0 flex flex-col p-5 bg-white rounded-lg shadow-xl top-7 w-72 gap-y-4"
+							>
+								<div
+									class="flex items-center gap-3"
+									@click.stop="showExtendEndDateModal = true"
+								>
+									<Icon
+										name="heroicons:calendar-days-16-solid"
+										class="text-2xl shrink-0"
+									/>
+									<p class="text-sm">Extend Deadline</p>
+								</div>
+								<button
+									@click.stop="showRemoveFromFeaturedModal = true"
+									class="flex items-center gap-3"
+								>
+									<Icon
+										name="heroicons:minus-16-solid"
+										class="text-2xl shrink-0"
+									/>
+									<p class="text-sm text-primary-600 whitespace-nowrap">
+										Remove from {{ featuredPlaceValue(featuredValue)?.name }}
+									</p>
+								</button>
+							</div>
+						</div>
+					</div>
+					<!-- status -->
+					<div class="flex flex-wrap gap-x-4 gap-y-2">
+						<div class="px-2 py-1 bg-blue-300 rounded-sm">
+							<p class="font-medium capitalize text-primary-600">
+								{{ place.type?.toLowerCase() }}
+							</p>
+						</div>
+						<div
+							v-for="featuredType in place?.featured_places"
+							class="px-2 py-1 rounded-sm"
+							:class="
+								featuredType.isActive
+									? 'bg-green-200 text-primary-600'
+									: 'bg-primary-100 text-primary-600'
+							"
+						>
+							<p class="font-medium">
+								{{
+									featuredPlaceValue(featuredType.featured_place_type?.value)
+										.name
+								}}
+							</p>
+						</div>
+					</div>
+				</div>
+			</div>
+		</div>
+		<!-- -------------------Body ---------------------------->
+		<div class="flex flex-col gap-4 my-2 text-sheger-gray-100">
+			<!----------------- Rating , Review   and Like -------------->
+			<CommonReviewRatingLike
+				:rating="place.place_aggregate_summary?.avgRating || 0"
+				:like="place.place_aggregate_summary?.sumLikes || 0"
+				:review="place.placeReviewsAggregate?.aggregate?.count || 0"
+			></CommonReviewRatingLike>
+			<!-------------------- Location------------------ -->
+			<div v-if="mainLocation" class="flex items-center gap-3">
+				<Icon name="carbon:map" class="w-5 h-5" />
 
-        <span class="underline decoration-sheger_brown-200 leading"
-          >{{ mainLocation.area?.name }}, {{ mainLocation.city?.name }}</span
-        >
-      </div>
-      <!-- Tags -->
-      <div class="flex flex-wrap pb-4 secondary-text">
-        <p
-          v-for="placeTag in place.placeTags"
-          :key="placeTag.tag.id"
-          class="pr-2"
-        >
-          #{{ placeTag.tag.title }}
-        </p>
-      </div>
-      <!-- menu update -->
+				<span class="underline decoration-sheger_brown-200 leading"
+					>{{ mainLocation.area?.name }}, {{ mainLocation.city?.name }}</span
+				>
+			</div>
+			<!-- Tags -->
+			<div class="flex flex-wrap pb-4 secondary-text">
+				<p
+					v-for="placeTag in place.placeTags"
+					:key="placeTag.tag.id"
+					class="pr-2"
+				>
+					#{{ placeTag.tag.title }}
+				</p>
+			</div>
+			<!-- menu update -->
 
-      <div v-if="getFeaturedPlace()" class="flex items-center gap-3">
-        <Icon name="carbon:calendar" class="w-5 h-5" />
-        <p class="">
-          {{ getFeaturedPlace()?.startDate }} to
-          {{ getFeaturedPlace()?.endDate }}
-        </p>
-      </div>
+			<div v-if="getFeaturedPlace()" class="flex items-center gap-3">
+				<Icon name="carbon:calendar" class="w-5 h-5" />
+				<p class="">
+					{{ getFeaturedPlace()?.startDate }} to
+					{{ getFeaturedPlace()?.endDate }}
+				</p>
+			</div>
 
-      <!-- last payed -->
-      <div class="flex items-center gap-3">
-        <Icon name="carbon:calendar" class="w-5 h-5" />
-        <p class="">Last payed: 2 days ago</p>
-      </div>
+			<!-- last payed -->
+			<div class="flex items-center gap-3">
+				<Icon name="carbon:calendar" class="w-5 h-5" />
+				<p class="">Last payed: 2 days ago</p>
+			</div>
 
-      <!-- --------------------Award ----------------->
-      <div v-if="place.shegerRecommendation" class="flex items-center gap-3">
-        <Icon name="uil:award-alt" class="w-5 h-5" />
-        <p class="">{{ place.shegerRecommendation?.title }}</p>
-      </div>
-    </div>
-  </div>
+			<!-- --------------------Award ----------------->
+			<div v-if="place.shegerRecommendation" class="flex items-center gap-3">
+				<Icon name="uil:award-alt" class="w-5 h-5" />
+				<p class="">{{ place.shegerRecommendation?.title }}</p>
+			</div>
+		</div>
+	</div>
 </template>
--- pages/app/places/register-new-place.vue
+++ pages/app/places/register-new-place.vue
@@ -2,103 +2,98 @@
 const placeType = ref("");
 
 definePageMeta({
-  layout: "engagement",
+	layout: "engagement",
 });
 </script>
 <template>
-  <div>
-    <!-- Top -->
+	<div>
+		<div class="flex justify-between h-20 my-4">
+			<div class="stepper-button rounded-tl-lg !bg-primary-300">Basic Info</div>
+			<div
+				class="stepper-button"
+				:class="
+					currentStep == 1
+						? '!bg-primary-300'
+						: currentStep > 1
+						? '!bg-primary-600 text-white'
+						: ''
+				"
+			>
+				Location
+			</div>
 
-    <!-- stepper -->
-    {{ placeType }} place type
+			<div
+				class="stepper-button"
+				:class="
+					currentStep == 2
+						? '!bg-primary-300'
+						: currentStep > 2
+						? '!bg-primary-600 text-white'
+						: ''
+				"
+			>
+				Menu
+			</div>
+			<div
+				class="stepper-button"
+				:class="
+					currentStep == 3
+						? '!bg-primary-300'
+						: currentStep > 3
+						? '!bg-primary-600 text-white'
+						: ''
+				"
+			>
+				Pictures
+			</div>
 
-    <div class="flex justify-between h-20 my-4">
-      <div class="stepper-button rounded-tl-lg !bg-primary-300">Basic Info</div>
-      <div
-        class="stepper-button"
-        :class="
-          currentStep == 1
-            ? '!bg-primary-300'
-            : currentStep > 1
-            ? '!bg-primary-600 text-white'
-            : ''
-        "
-      >
-        Location
-      </div>
+			<!-- -------------If place type is HOTELS-------------- -->
+			<div
+				v-if="placeType == 'Hotels'"
+				class="stepper-button"
+				:class="
+					currentStep == 4
+						? '!bg-primary-300'
+						: currentStep > 4
+						? '!bg-primary-600 text-white'
+						: ''
+				"
+			>
+				Amenities
+			</div>
 
-      <div
-        class="stepper-button"
-        :class="
-          currentStep == 2
-            ? '!bg-primary-300'
-            : currentStep > 2
-            ? '!bg-primary-600 text-white'
-            : ''
-        "
-      >
-        Menu
-      </div>
-      <div
-        class="stepper-button"
-        :class="
-          currentStep == 3
-            ? '!bg-primary-300'
-            : currentStep > 3
-            ? '!bg-primary-600 text-white'
-            : ''
-        "
-      >
-        Pictures
-      </div>
+			<!-- -------------If place type is CATERINGS-------------- -->
 
-      <!-- -------------If place type is HOTELS-------------- -->
-      <div
-        v-if="placeType == 'Hotels'"
-        class="stepper-button"
-        :class="
-          currentStep == 4
-            ? '!bg-primary-300'
-            : currentStep > 4
-            ? '!bg-primary-600 text-white'
-            : ''
-        "
-      >
-        Amenities
-      </div>
-
-      <!-- -------------If place type is CATERINGS-------------- -->
-
-      <div
-        v-if="placeType == 'Caterings'"
-        class="stepper-button"
-        :class="
-          currentStep == 5
-            ? '!bg-primary-300'
-            : currentStep > 5
-            ? '!bg-primary-600 text-white'
-            : ''
-        "
-      >
-        Portfolio
-      </div>
-      <div
-        class="rounded-tr-lg stepper-button"
-        :class="
-          currentStep == 6
-            ? '!bg-primary-300'
-            : currentStep > 6
-            ? '!bg-primary-600 text-white'
-            : ''
-        "
-      >
-        Pricing Plan
-      </div>
-    </div>
-    <div class="min-h-[70%] border flex flex-col justify-between px-5 py-2">
-      <div class="h-full">
-        <PlacesFormBasicInfo v-model="placeType" />
-      </div>
-    </div>
-  </div>
+			<div
+				v-if="placeType == 'Caterings'"
+				class="stepper-button"
+				:class="
+					currentStep == 5
+						? '!bg-primary-300'
+						: currentStep > 5
+						? '!bg-primary-600 text-white'
+						: ''
+				"
+			>
+				Portfolio
+			</div>
+			<div
+				class="rounded-tr-lg stepper-button"
+				:class="
+					currentStep == 6
+						? '!bg-primary-300'
+						: currentStep > 6
+						? '!bg-primary-600 text-white'
+						: ''
+				"
+			>
+				Pricing Plan
+			</div>
+		</div>
+		<div class="min-h-[70%] border flex flex-col justify-between px-5 py-2">
+			<div class="h-full">
+				<PlacesFormBasicInfo v-model="placeType" />
+			</div>
+		</div>
+	</div>
 </template>
